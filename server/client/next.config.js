3c52f7526de9a81b23c41fab46bc65945a8cea53
Moustamid <karim1992moustamid@gmail.com>
2021-03-10 16:26:11 -0600

    adding some styles

--- server/client/next.config.js
+++ server/client/next.config.js
@@ -8,3 +8,22 @@ module.exports = {
     ];
   },
 };
+
+/*
+
+NOTE: 
+
+- http://localhost:5000/auth/google : 
+ will not work in by def in developement : 
+
+- Solution :
+ https://nextjs.org/docs/api-reference/next.config.js/rewrites
+
+- in production : the react server does not exist anymore ...
+http://localhost:5000/auth/google/callback?
+code=4
+%2F0AY0e-g4N2gzXCTL0uV56L9Suf0d66q2NPoOyZJqNDCytxGpUcK4vB8CyfLW9Uc4ekgYNXA
+&scope=email+profile+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+openid&authuser=0&prompt=consent#
+
+
+*/
--- server/client/pages/index.js
+++ server/client/pages/index.js
@@ -4,20 +4,24 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className="container">
+      <img className="Next" src="/next.svg" />
       <h1>Welcom to the Nextjs client of my boilerplate</h1>
-      <button>
-        <Link href="/auth/google/" as="/auth/google/">
-          <a>Visit /team</a>
-        </Link>
-      </button>
+
+      <div class="google-btn">
+        <div class="google-icon-wrapper">
+          <Link href="/auth/google/" as="/auth/google/">
+            <a>
+              <img
+                class="google-icon"
+                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+              />
+            </a>
+          </Link>
+        </div>
+        <p class="btn-text">
+          <b>Sign in with google</b>
+        </p>
+      </div>
     </div>
   );
 }
-
-/*
-
-NOTE: http://localhost:5000/auth/google , will not work in production : 
-
-- Solution : 
-
-*/
